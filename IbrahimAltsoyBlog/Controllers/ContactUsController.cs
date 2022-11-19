472c6e8fe451c09b81afe5557af7658633a0b348
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2022-11-19 18:28:01 +0300

    19.11.2022 Homework

---
+++ IbrahimAltsoyBlog/Controllers/ContactUsController.cs
@@ -0,0 +1,19 @@
+﻿using IbrahimAltsoyBlog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IbrahimAltsoyBlog.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(ContactUs contact)
+        {
+            return View();
+        }
+    }
+}
